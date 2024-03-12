a18161f43994231ecef139771c3110d7a077f6f1
vpavliukUA <Vasyl.Pavliuk@acoustic.co>
2024-03-12 21:26:49 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useRef, useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const inputRef = useRef(null);
+
+  const handleButtonClick = () => {
+    console.log("Your input value using useState:", input);
+    console.log("Your input value using ref:", inputRef.current.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        ref={inputRef}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={handleButtonClick}>Click me to show input data!</button>
     </div>
   );
 }
